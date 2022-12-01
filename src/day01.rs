0a79a893037a4aa7b02f28d2d634ff562d403649
Mikael Auno <mikael@auno.se>
2022-12-01 07:51:55 +0100

    Finished 2022/1b

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
 #[aoc_generator(day1)]
 fn parse(input: &str) -> Vec<Vec<u32>> {
@@ -16,6 +17,16 @@ fn part1(input: &Vec<Vec<u32>>) -> u32 {
         .unwrap_or_default()
 }
 
+#[aoc(day1, part2)]
+fn part2(input: &Vec<Vec<u32>>) -> u32 {
+    input.into_iter()
+        .map(|calorie_counts| calorie_counts.into_iter().sum::<u32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +34,10 @@ mod tests {
     #[test]
     fn part1_example1() {
         assert_eq!(24000, part1(&parse(include_str!("../input/2022/day1.part1.test.24000.txt"))));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(45000, part2(&parse(include_str!("../input/2022/day1.part2.test.45000.txt"))));
     }
 }
