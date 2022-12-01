7e03ddd00e8abe96aa37c3296e8b6a3c229e3122
Mikael Auno <mikael@auno.se>
2022-12-01 07:46:41 +0100

    Finished 2022/1a

---
+++ src/day01.rs
@@ -0,0 +1,27 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn parse(input: &str) -> Vec<Vec<u32>> {
+    input
+        .split("\n\n")
+        .map(|line| line.lines().map(|line| line.parse().unwrap()).collect())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &Vec<Vec<u32>>) -> u32 {
+    input.into_iter()
+        .map(|calorie_counts| calorie_counts.into_iter().sum())
+        .max()
+        .unwrap_or_default()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example1() {
+        assert_eq!(24000, part1(&parse(include_str!("../input/2022/day1.part1.test.24000.txt"))));
+    }
+}
