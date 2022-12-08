63ea762ba97f5f44dd3b719d73ed2ce72416a23a
Mikael Auno <mikael@auno.se>
2022-12-08 08:15:40 +0100

    Finished 2022/8b

--- src/day08.rs
+++ src/day08.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day8)]
@@ -37,6 +38,29 @@ fn part1(input: &Vec<Vec<u32>>) -> usize {
     count
 }
 
+#[aoc(day8, part2)]
+fn part2(input: &Vec<Vec<u32>>) -> usize {
+    let height = input.len();
+    let width = input[0].len();
+
+    let mut best_score = 0;
+
+    for row in 0..height {
+        for col in 0..width {
+            let distances = vec![
+                min((0..row).rev().take_while(|&i| input[i][col] < input[row][col]).count() + 1, row),
+                min(((row+1)..height).take_while(|&i| input[i][col] < input[row][col]).count() + 1, height - row - 1),
+                min((0..col).rev().take_while(|&i| input[row][i] < input[row][col]).count() + 1, col),
+                min(((col+1)..width).take_while(|&i| input[row][i] < input[row][col]).count() + 1, width - col - 1),
+            ];
+            let score = distances.iter().product();
+            best_score = max(best_score, score);
+        }
+    }
+
+    best_score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,7 +71,17 @@ mod tests {
     }
 
     #[test]
+    fn part2_example1() {
+        assert_eq!(8, part2(&parse(include_str!("../input/2022/day8.part2.test.8.txt"))));
+    }
+
+    #[test]
     fn part1_input() {
         assert_eq!(1859, part1(&parse(include_str!("../input/2022/day8.txt"))));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(332640, part2(&parse(include_str!("../input/2022/day8.txt"))));
     }
 }
