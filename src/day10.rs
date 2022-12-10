88b37a822e38b85b92f1393f8d2f78c903eedf46
Mikael Auno <mikael@auno.se>
2022-12-10 08:27:05 +0100

    Finished 2022/10a

---
+++ src/day10.rs
@@ -0,0 +1,72 @@
+use std::str::FromStr;
+use aoc_runner_derive::{aoc, aoc_generator};
+use anyhow::{Result, bail, Context};
+use crate::day10::Instruction::{AddX, NoOp};
+
+#[derive(Debug, Copy, Clone)]
+enum Instruction {
+    NoOp,
+    AddX(i32),
+}
+
+impl FromStr for Instruction {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Instruction> {
+        let mut parts = s.split(' ');
+        match parts.next() {
+            Some("noop") => Ok(NoOp),
+            Some("addx") => Ok(AddX(parts.next().context("Missing operand")?.parse()?)),
+            Some(_) | None => bail!("Malformed input line: {}", s),
+        }
+    }
+}
+
+#[aoc_generator(day10)]
+fn parse(input: &str) -> Result<Vec<Instruction>> {
+    input
+        .lines()
+        .map(&str::parse)
+        .collect()
+}
+
+#[aoc(day10, part1)]
+fn part1(input: &[Instruction]) -> i32 {
+    input.iter()
+        .flat_map(|instruction| match *instruction {
+            NoOp => vec![NoOp],
+            AddX(v) => vec![NoOp, AddX(v)],
+        })
+        .take(221)
+        .enumerate()
+        .map(|(cycle, instruction)| (cycle + 1, instruction))
+        .fold((1, 0), |(mut x, mut cumulative_strength), (cycle, instruction)| {
+            cumulative_strength += match cycle % 40 {
+                20 => (cycle as i32) * x,
+                _ => 0,
+            };
+
+            x += match instruction {
+                NoOp => 0,
+                AddX(v) => v,
+            };
+
+            (x, cumulative_strength)
+        })
+        .1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example1() {
+        assert_eq!(13140, part1(&parse(include_str!("../input/2022/day10.part1.test.13140.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(13220, part1(&parse(include_str!("../input/2022/day10.txt")).unwrap()));
+    }
+}
