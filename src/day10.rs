2ff5fecdea0c356dd69bc8fbd5108ff5847d1068
Mikael Auno <mikael@auno.se>
2022-12-10 09:55:30 +0100

    Finished 2022/10b

--- src/day10.rs
+++ src/day10.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::{Result, bail, Context};
 use crate::day10::Instruction::{AddX, NoOp};
+use crate::ocr::ocr;
 
 #[derive(Debug, Copy, Clone)]
 enum Instruction {
@@ -56,6 +57,37 @@ fn part1(input: &[Instruction]) -> i32 {
         .1
 }
 
+fn execute_instructions(input: &[Instruction]) -> String {
+    input.iter()
+        .flat_map(|instruction| match *instruction {
+            NoOp => vec![NoOp],
+            AddX(v) => vec![NoOp, AddX(v)],
+        })
+        .enumerate()
+        .map(|(cycle, instruction)| (cycle, instruction))
+        .fold((1, vec!['.'; 240]), |(x, mut screen), (cycle, instruction)| {
+            if ((x - 1)..=(x + 1)).contains(&((cycle % 40) as i32)) {
+                screen[cycle] = '#';
+            }
+
+            let x = match instruction {
+                NoOp => x,
+                AddX(v) => x + v,
+            };
+
+            (x, screen)
+        })
+        .1
+        .iter()
+        .collect::<String>()
+}
+
+#[aoc(day10, part2)]
+fn part2(input: &[Instruction]) -> String {
+    let screen = execute_instructions(input);
+    ocr('#', &screen).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +100,24 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(13220, part1(&parse(include_str!("../input/2022/day10.txt")).unwrap()));
+    }
+
+    #[test]
+    fn execute_instructions_example1() {
+        let expected = [
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######.....",
+        ].join("");
+
+        assert_eq!(expected, execute_instructions(&parse(include_str!("../input/2022/day10.part2.test.124.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!("RUAKHBEK", part2(&parse(include_str!("../input/2022/day10.txt")).unwrap()));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use aoc_runner_derive::aoc_lib;
 
+mod ocr;
 mod day01;
 mod day02;
 mod day03;
