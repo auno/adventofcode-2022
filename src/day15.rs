255b727fa66e4687872928e5b5fe0c79487e1402
Mikael Auno <mikael@auno.se>
2022-12-15 10:19:11 +0100

    Finished 2022/15b

--- src/day15.rs
+++ src/day15.rs
@@ -4,24 +4,24 @@ use anyhow::Result;
 use itertools::Itertools;
 use scan_fmt::scan_fmt;
 
-type Coordinate = (i32, i32);
+type Coordinate = (i64, i64);
 
 #[aoc_generator(day15)]
 fn parse(input: &str) -> Result<Vec<(Coordinate, Coordinate)>> {
     input
         .lines()
         .map(|line| {
-            let (sx, sy, bx, by) = scan_fmt!(line, "Sensor at x={}, y={}: closest beacon is at x={}, y={}", i32, i32, i32, i32)?;
+            let (sx, sy, bx, by) = scan_fmt!(line, "Sensor at x={}, y={}: closest beacon is at x={}, y={}", i64, i64, i64, i64)?;
             Ok(((sx, sy), (bx, by)))
         })
         .collect()
 }
 
-fn distance((ax, ay): &Coordinate, (bx, by): &Coordinate) -> i32 {
+fn distance((ax, ay): &Coordinate, (bx, by): &Coordinate) -> i64 {
     (ax - bx).abs() + (ay - by).abs()
 }
 
-fn solve1(input: &[(Coordinate, Coordinate)], y: i32) -> i32 {
+fn find_and_normalize_ranges(input: &[(Coordinate, Coordinate)], y: i64) -> Vec<(i64, i64)> {
     let mut excluded = input
         .iter()
         .copied()
@@ -54,14 +54,32 @@ fn solve1(input: &[(Coordinate, Coordinate)], y: i32) -> i32 {
     }
 
     excluded
+}
+
+fn solve1(input: &[(Coordinate, Coordinate)], y: i64) -> i64 {
+    find_and_normalize_ranges(input, y)
         .iter()
         .map(|(a, b)| b - a)
         .sum()
 }
 
 #[aoc(day15, part1)]
-fn part1(input: &[(Coordinate, Coordinate)]) -> i32 {
+fn part1(input: &[(Coordinate, Coordinate)]) -> i64 {
     solve1(input, 2000000)
+}
+
+fn solve2(input: &[(Coordinate, Coordinate)], max: i64) -> Option<i64> {
+    (0..=max)
+        .map(|y| find_and_normalize_ranges(input, y))
+        .enumerate()
+        .find(|(_, ranges)| ranges.len() > 1)
+        .map(|(y, ranges)| (ranges[0].1 as i64 + 1, y as i64))
+        .map(|(x, y)| x * 4000000 + y)
+}
+
+#[aoc(day15, part2)]
+fn part2(input: &[(Coordinate, Coordinate)]) -> i64 {
+    solve2(input, 4000000).unwrap()
 }
 
 #[cfg(test)]
@@ -75,6 +93,16 @@ mod tests {
 
     #[test]
     fn part1_input() {
-        assert_eq!(4665948, solve1(&parse(include_str!("../input/2022/day15.txt")).unwrap(), 2000000));
+        assert_eq!(4665948, part1(&parse(include_str!("../input/2022/day15.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(Some(56000011), solve2(&parse(include_str!("../input/2022/day15.part2.test.56000011.txt")).unwrap(), 20));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(13543690671045, part2(&parse(include_str!("../input/2022/day15.txt")).unwrap()));
     }
 }
