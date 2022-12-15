8de711cce1567c893b20c83d5094ab820e98def0
Mikael Auno <mikael@auno.se>
2022-12-15 15:13:10 +0100

    Refactored and optimized day 15

--- src/day15.rs
+++ src/day15.rs
@@ -3,6 +3,7 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::Result;
 use itertools::Itertools;
 use scan_fmt::scan_fmt;
+use rayon::prelude::*;
 
 type Coordinate = (i64, i64);
 
@@ -21,8 +22,8 @@ fn distance((ax, ay): &Coordinate, (bx, by): &Coordinate) -> i64 {
     (ax - bx).abs() + (ay - by).abs()
 }
 
-fn find_and_normalize_ranges(input: &[(Coordinate, Coordinate)], y: i64) -> Vec<(i64, i64)> {
-    let mut excluded = input
+fn reachable_ranges(input: &[(Coordinate, Coordinate)], y: i64) -> Vec<(i64, i64)> {
+    input
         .iter()
         .copied()
         .filter_map(|((sx, sy), beacon)| {
@@ -33,34 +34,36 @@ fn find_and_normalize_ranges(input: &[(Coordinate, Coordinate)], y: i64) -> Vec<
                 return None;
             }
 
-            Some((sx - (d - dy), sx + (d - dy)))
+            Some((sx - (d - dy), sx + (d - dy) + 1))
         })
         .sorted()
-        .collect::<Vec<_>>();
+        .fold(vec![], |mut acc, (a, b)| {
+            if let Some((_, pb)) = acc.last_mut() {
+                if a <= *pb {
+                    *pb = max(b, *pb);
+                    return acc;
+                }
+            }
 
-    let mut i = 1;
-
-    while i < excluded.len() {
-        let (a1, b1) = excluded[i - 1];
-        let (a2, b2) = excluded[i];
-
-        if a2 <= b1 {
-            excluded[i - 1] = (a1, max(b1, b2));
-            excluded.remove(i);
-            continue;
-        }
-
-        i += 1;
-    }
-
-    excluded
+            acc.push((a, b));
+            acc
+        })
 }
 
 fn solve1(input: &[(Coordinate, Coordinate)], y: i64) -> i64 {
-    find_and_normalize_ranges(input, y)
+    let num_reachable = reachable_ranges(input, y)
         .iter()
         .map(|(a, b)| b - a)
-        .sum()
+        .sum::<i64>();
+    let num_beacons = input
+        .iter()
+        .filter(|(_, (_, cy))| *cy == y)
+        .map(|(_, (cx, _))| cx)
+        .unique()
+        .count()
+        as i64;
+
+    num_reachable - num_beacons
 }
 
 #[aoc(day15, part1)]
@@ -70,10 +73,10 @@ fn part1(input: &[(Coordinate, Coordinate)]) -> i64 {
 
 fn solve2(input: &[(Coordinate, Coordinate)], max: i64) -> Option<i64> {
     (0..=max)
-        .map(|y| find_and_normalize_ranges(input, y))
-        .enumerate()
-        .find(|(_, ranges)| ranges.len() > 1)
-        .map(|(y, ranges)| (ranges[0].1 as i64 + 1, y as i64))
+        .into_par_iter()
+        .map(|y| (y, reachable_ranges(input, y)))
+        .find_any(|(_, ranges)| ranges.len() > 1)
+        .map(|(y, ranges)| (ranges[0].1 as i64, y as i64))
         .map(|(x, y)| x * 4000000 + y)
 }
 
