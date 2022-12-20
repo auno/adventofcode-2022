69dd8a982a1794b933f358bbd90e7f691bb531cd
Mikael Auno <mikael@auno.se>
2022-12-20 18:42:54 +0100

    Finished 2022/20b

--- src/day20.rs
+++ src/day20.rs
@@ -1,45 +1,40 @@
 use std::collections::VecDeque;
 use aoc_runner_derive::{aoc, aoc_generator};
-use anyhow::Result;
 
 #[aoc_generator(day20)]
-fn parse(input: &str) -> Vec<i32> {
+fn parse(input: &str) -> Vec<i64> {
     input
         .lines()
         .map(|line| line.parse().unwrap())
         .collect()
 }
 
-fn mix1(indices: &mut VecDeque<i32>, input: &[i32], original_index: i32) {
-    if input[original_index as usize] == 0 {
-        return;
+fn solve(input: &[i64], multiplier: i64, rounds: usize) -> i64 {
+    let len = input.len();
+    let input = input.iter().map(|n| n * multiplier).collect::<Vec<_>>();
+    let mut indices = (0..len).collect::<VecDeque<_>>();
+
+    for _ in 0..rounds {
+        for i in 0..len {
+            let from = indices.iter().position(|candidate| *candidate == i).unwrap();
+            let to = (from as i64 + input[i]).rem_euclid(len as i64 - 1) as usize;
+            let temp = indices.remove(from).unwrap();
+            indices.insert(to, temp);
+        }
     }
 
-    let len = input.len() as i32;
-
-    let i = indices.iter().position(|candidate| *candidate == original_index).unwrap() as i32;
-    let mut next_i = i + input[original_index as usize];
-
-    if next_i <= 0 || next_i >= len {
-        next_i += input[original_index as usize].signum();
-        next_i = next_i.rem_euclid(len);
-    }
-
-    let temp = indices.remove(i as usize).unwrap();
-    indices.insert(next_i as usize, temp);
+    let pos0 = indices.iter().position(|&i| input[i] == 0).unwrap();
+    input[indices[(pos0 + 1000) % len]] + input[indices[(pos0 + 2000) % len]] + input[indices[(pos0 + 3000) % len]]
 }
 
 #[aoc(day20, part1)]
-fn part1(input: &[i32]) -> i32 {
-    let len = input.len() as i32;
-    let mut indices = (0..len).collect::<VecDeque<_>>();
+fn part1(input: &[i64]) -> i64 {
+    solve(input, 1, 1)
+}
 
-    for original_index in 0..len {
-        mix1(&mut indices, input, original_index)
-    }
-
-    let pos0 = indices.iter().position(|i| input[*i as usize] == 0).unwrap();
-    input[indices[(pos0 + 1000) % input.len()] as usize] + input[indices[(pos0 + 2000) % input.len()] as usize] + input[indices[(pos0 + 3000) % input.len()] as usize]
+#[aoc(day20, part2)]
+fn part2(input: &[i64]) -> i64 {
+    solve(input, 811589153, 10)
 }
 
 #[cfg(test)]
@@ -52,61 +47,17 @@ mod tests {
     }
 
     #[test]
-    fn mix1_example1() {
-        let input = [1, 2, -3, 3, -2, 0, 4];
-        let mut indices = VecDeque::from([0, 1, 2, 3, 4, 5, 6]);
-
-        mix1(&mut indices, &input, 0);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[2, 1, -3, 3, -2, 0, 4], actual.as_slice());
-
-        mix1(&mut indices, &input, 1);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, -3, 2, 3, -2, 0, 4], actual.as_slice());
-
-        mix1(&mut indices, &input, 2);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, 2, 3, -2, -3, 0, 4], actual.as_slice());
-
-        mix1(&mut indices, &input, 3);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, 2, -2, -3, 0, 3, 4], actual.as_slice());
-
-        mix1(&mut indices, &input, 4);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, 2, -3, 0, 3, 4, -2], actual.as_slice());
-
-        mix1(&mut indices, &input, 5);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, 2, -3, 0, 3, 4, -2], actual.as_slice());
-
-        mix1(&mut indices, &input, 6);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[1, 2, -3, 4, 0, 3, -2], actual.as_slice());
-    }
-
-    #[test]
-    fn mix1_example2() {
-        let input = [4, 5, 6, 1, 7, 8, 9];
-        let mut indices = VecDeque::from([0, 1, 2, 3, 4, 5, 6]);
-
-        mix1(&mut indices, &input, 3);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[4, 5, 6, 7, 1, 8, 9], actual.as_slice());
-    }
-
-    #[test]
-    fn mix1_example3() {
-        let input = [4, -2, 5, 6, 7, 8, 9];
-        let mut indices = VecDeque::from([0, 1, 2, 3, 4, 5, 6]);
-
-        mix1(&mut indices, &input, 1);
-        let actual = indices.iter().map(|&i| input[i as usize]).collect::<Vec<_>>();
-        assert_eq!(&[4, 5, 6, 7, 8, -2, 9], actual.as_slice());
-    }
-
-    #[test]
     fn part1_input() {
         assert_eq!(7713, part1(&parse(include_str!("../input/2022/day20.txt"))));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(1623178306, part2(&parse(include_str!("../input/2022/day20.part2.test.1623178306.txt"))));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(1664569352803, part2(&parse(include_str!("../input/2022/day20.txt"))));
     }
 }
