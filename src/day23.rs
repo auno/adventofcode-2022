ac1a5ca162470105ebc9e0ef8d495302842a8335
Mikael Auno <mikael@auno.se>
2022-12-23 10:03:41 +0100

    Finished 2022/23b

--- src/day23.rs
+++ src/day23.rs
@@ -45,36 +45,46 @@ fn parse(input: &str) -> HashSet<(i32, i32)> {
         .collect()
 }
 
+fn step(occupied: &mut HashSet<(i32, i32)>, directions: [Direction; 4], round: usize) -> bool {
+    let mut moves = HashMap::new();
+
+    for &(x, y) in occupied.iter() {
+        if directions.into_iter().flat_map(|d| d.neighbors((x, y))).unique().all(|n| !occupied.contains(&n)) {
+            continue;
+        }
+
+        for direction in (0..4).map(|i| directions[(round + i) % 4]) {
+            if direction.neighbors((x, y)).iter().all(|n| !occupied.contains(n)) {
+                let target = direction.translate((x, y));
+                moves.entry(target).or_insert_with(Vec::new).push((x, y));
+                break;
+            }
+        }
+    }
+
+    if moves.is_empty() {
+        return true;
+    }
+
+    for (target, sources) in moves {
+        if sources.len() != 1 {
+            continue;
+        }
+
+        occupied.insert(target);
+        occupied.remove(&sources[0]);
+    }
+
+    false
+}
+
 #[aoc(day23, part1)]
 fn part1(input: &HashSet<(i32, i32)>) -> usize {
     let mut occupied = input.clone();
     let directions = [ North, South, West, East ];
 
     for round in 0..10 {
-        let mut moves = HashMap::new();
-
-        for &(x, y) in &occupied {
-            if directions.into_iter().flat_map(|d| d.neighbors((x, y))).unique().all(|n| !occupied.contains(&n)) {
-                continue;
-            }
-
-            for direction in (0..4).map(|i| directions[(round + i) % 4]) {
-                if direction.neighbors((x, y)).iter().all(|n| !occupied.contains(n)) {
-                    let target = direction.translate((x, y));
-                    moves.entry(target).or_insert_with(Vec::new).push((x, y));
-                    break;
-                }
-            }
-        }
-
-        for (target, sources) in moves {
-            if sources.len() != 1 {
-                continue;
-            }
-
-            occupied.insert(target);
-            occupied.remove(&sources[0]);
-        }
+        step(&mut occupied, directions, round);
     }
 
     let (min_x, min_y, max_x, max_y) = (
@@ -88,6 +98,20 @@ fn part1(input: &HashSet<(i32, i32)>) -> usize {
     let height = (max_y - min_y + 1) as usize;
 
     width * height - occupied.len()
+}
+
+#[aoc(day23, part2)]
+fn part2(input: &HashSet<(i32, i32)>) -> usize {
+    let mut occupied = input.clone();
+    let directions = [ North, South, West, East ];
+
+    for round in 0.. {
+        if step(&mut occupied, directions, round) {
+            return round + 1;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -107,5 +131,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(3800, part1(&parse(include_str!("../input/2022/day23.txt"))));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(20, part2(&parse(include_str!("../input/2022/day23.part2.test.20.txt"))));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(916, part2(&parse(include_str!("../input/2022/day23.txt"))));
     }
 }
